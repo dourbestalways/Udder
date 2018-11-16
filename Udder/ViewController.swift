f4e812c204f8445131150d539865ef1c052edc6a
Rax Kim <dourbestalways@gmail.com>
2018-11-16 23:05:01 +0900

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Sung Hwan Kim on 16/11/2018.
+//  Copyright © 2018 Sung Hwan Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
